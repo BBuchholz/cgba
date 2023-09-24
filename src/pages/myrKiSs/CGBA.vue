fc15d512032301fab32df2a0df62b1a264b45e0e
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-24 10:06:02 -0500

    Add CGBA intro in progress

---
+++ src/pages/myrKiSs/CGBA.vue
@@ -0,0 +1,68 @@
+<template>
+  <p class="academic">
+    "During the Satanic Panic, parents were heavily concerned with the largely unfounded myths that their children were running around blending fantasy with reality in dangerous ways..." - Dr. XXX YYY, professor of ZZZ WWW
+  </p>
+  <p class="second-tier">
+    "What if you did that but not in a Criminal way? like harnessed the imaginative power of LARP without the dangers of getting arrested, while still keeping it much more real than nerf swords and costumes?" - persona B
+  </p>
+  <p class="conservative">
+    "wait? you're not really advocating for doing the exact thing parents freaked about in the 80s and 90s are you?" - persona C
+  </p>
+  <p class="sorcerer">
+    "yeah... we're doing that..." - persona D
+  </p>
+  <p class="mesa">
+    this a WorkIng Progress, with dev notes hosted at WorkShop Portfolio BAOLG (I -> Sh, cf. Hebrew ISh) (visit with passCode "CGBA" to see public comments)
+  </p>
+  <div>
+    <img
+      class="card"
+      src="https://madamadam.s3.us-east-2.amazonaws.com/CGBA-13c9.png"
+    >
+  </div>
+  <p class="mesa">
+    card attribution info here
+  </p>
+</template>
+
+<style scoped>
+  .mesa {
+    margin: 40px;
+    border: solid;
+    border-color: purple;
+  }
+  .academic {
+    margin: 40px;
+    border: solid;
+    border-color: orange;
+  }
+  .second-tier {
+    margin: 40px;
+    border: solid;
+    border-color: yellow;
+  }
+  .conservative {
+    margin: 40px;
+    border: solid;
+    border-color: blue;
+  }
+  .sorcerer {
+    margin: 40px;
+    border: solid;
+    border-color: red;
+  }
+
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
